eb74ffaf7e04901eb715df61a4a806bafac6483d
roxolanazz08 <roksolana.zaikina.oi.2024@lpnu.ua>
2025-09-21 19:20:18 +0300

    first commit

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"FibonacciNumbers","l":"Fibonacci"},{"p":"FibonacciNumbers","l":"Main"}];updateSearchResults();
