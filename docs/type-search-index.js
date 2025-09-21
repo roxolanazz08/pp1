9ec61fc4acf5d8195a6713860239954d6901c917
roxolanazz08 <roksolana.zaikina.oi.2024@lpnu.ua>
2025-09-21 19:38:54 +0300

    Add JavaDoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"FibonacciNumbers","l":"Fibonacci"},{"p":"FibonacciNumbers","l":"Main"}];updateSearchResults();
